bfaba3e0dfdceafeff10eaf52a64db9fd4bba41b
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-11-03 10:51:56 -0300

    atualização

---
+++ Questões Regex/Exercícios.py
@@ -0,0 +1,29 @@
+import re
+
+#Questão 1==================================================================
+# msg = 'AAAAAAAAA'
+# regex= re.compile(r"A+")
+
+# if len(msg)%2==0 and\
+#     regex.fullmatch(msg):
+#         print(f"A mensagem {msg} tem tamanho par e é formada apenas por 'A'")
+#         print(regex.fullmatch(msg))
+        
+# elif len(msg)%2!=0:
+#     print("O tamanho da mensagem não é par")
+    
+# else:
+#     print("A msg não é formada apenas por 'A' ")
+
+
+# #Questão2==============================================
+# msg= "@...men%sag*em d3 t e#st)e"
+# regex= re.compile(r"[^A-Za-z\s]+")
+# saida = regex.sub("", msg)
+# print(saida)
+
+
+#Questão 3=================================
+
+
+
