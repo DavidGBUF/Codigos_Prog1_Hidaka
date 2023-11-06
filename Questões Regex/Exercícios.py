99614898bf7907e897bc3f0c46cf6201e678de30
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-11-06 10:23:22 -0300

    atualizaçao

--- Questões Regex/Exercícios.py
+++ Questões Regex/Exercícios.py
@@ -25,5 +25,53 @@ import re
 
 #Questão 3=================================
 
+# msg='nina2'
+# if len(msg)>=10 and\
+#     re.search(r"[\d]",msg) and \
+#     re.search(r"[A-Z]",msg) and\
+#     re.search(r"[a-z]",msg) and\
+#     len(re.findall(r"[!@#$%&*+=]",msg))>=3:
+#         print("MUito forte")
+        
+# elif len(msg)>=8 and\
+#     re.search(r"[\d]",msg) and \
+#     re.search(r"[A-Z]",msg) and\
+#     re.search(r"[a-z]",msg) and\
+#     re.search(r"[!@#$%&*+=]",msg):
+#         print("Forte")
+        
+# elif len(msg)>=6 and\
+#     re.search(r"[\d]",msg) and \
+#     re.search(r"[A-Z]",msg) and\
+#     re.search(r"[a-z]",msg):
+#         print("Média")
+        
+# elif len(msg)>=6:
+#         print("Fraca")
+# else:
+#     print("SENHA INVALIDA")
+        
+        
+#Questão5==================================================
+# msg='1000011'
+# regex= re.compile(r"[1 0]+")
+# if regex.fullmatch(msg) and len(re.findall(r"1", msg))>=3:
+#     print("sucesso")
 
 
+
+# #Questão6==========================================
+# msg='10101010101010'
+# regex= re.compile(r"[1 0]+")
+
+# if regex.fullmatch(msg) and not re.search(r"11", msg):
+#     print("sucesso")
+
+
+
+
+#Questão7============================
+msg='David R$Galhego de Nazare'
+regex= re.compile(r"\bR\$\w*")
+
+print(regex.findall(msg))
