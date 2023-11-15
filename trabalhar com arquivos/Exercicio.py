f900753940527cd538ac300aa8d70b78c2c60b42
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-11-15 18:10:06 -0300

    atualização

--- trabalhar com arquivos/Exercicio.py
+++ trabalhar com arquivos/Exercicio.py
@@ -1,5 +1,10 @@
 def menu():
-    return int(input("""[1] Para Cadastrar\n[2] Para Listar\n[3] Buscar por nome\n[*]Digite qualquer outra coisa para encerrar\nSua opção: """))
+    return int(input("""
+    [1] Para Cadastrar
+    [2] Para Listar
+    [3] Buscar por nome
+    [*] Digite qualquer outra coisa para encerrar
+    Sua opção: """))
 
 
 
@@ -9,22 +14,23 @@ def cadastrar():
     telefone= input("Digite o numero de telefone: ")
     email = input("Digite o e-mail: ")
     with open('./trabalhar com arquivos/BD_Cadastro.txt', 'a', encoding="utf-8") as f:
-        f.write(f"\n{nome}  , {telefone}  ,{email}")
+        f.write(f"\n{nome},{telefone} ,{email}")
         
 
-
+def formatar(linha):
+    import re
+    return re.sub("," , "\t\t\t", linha)
 
 def listar():
     with open('./trabalhar com arquivos/BD_Cadastro.txt', 'r',encoding="utf-8") as f:
-        lista = [linha for linha in f]
-        for i in lista:
-            print(i)
+        lista = [formatar(linha) for linha in f]
+        print(*lista)
 
 
 def buscar_por_nome():
     busca = input("Digite a busca: ")
     with open('./trabalhar com arquivos/BD_Cadastro.txt', "r", encoding= "utf-8") as f:
-        lista = [linha for linha in f if linha.upper().startswith(busca.upper())]
+        lista = [formatar(linha) for linha in f if linha.upper().startswith(busca.upper())]
         print(*lista)
 
 
